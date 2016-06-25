d15a5fa3eb6c0d156bc92c632303468a1c43f9c4
Andrew Lee <email": null>
2016-06-24 17:23:20 -0700

    only let the user see his own wishlist

--- app/controllers/wishlists_controller.rb
+++ app/controllers/wishlists_controller.rb
@@ -1,13 +1,15 @@
 class WishlistsController < ApplicationController
   def index
-    wishlists = Wishlist.all
+    wishlists = Wishlist.all.where(user_id: params[:user_id])
     render json: wishlists.to_json
   end
 
   def show
     wishlist = Wishlist.find(params[:id])
-    liked_places_ids = wishlist.places_wishlists.where(liked: true).pluck(:place_id)
-    liked_places = Place.find(liked_places_ids)
-    render json: liked_places.to_json
+    if wishlist.user_id == params[:user_id]
+      liked_places_ids = wishlist.places_wishlists.where(liked: true).pluck(:place_id)
+      liked_places = Place.find(liked_places_ids)
+      render json: liked_places.to_json
+    end
   end
 end

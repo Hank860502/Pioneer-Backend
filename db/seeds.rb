cdfb85a606bf885570b3f8489ca0f5ea986e8f2c
Andrew Lee <email": null>
2016-06-27 16:45:14 -0700

    update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,7 +17,7 @@ alcatraz = Place.create(
   latitude: 37.826986,
   longitude: -122.422972,
   rating: 4.9,
-  duration: 5,
+  duration: 300,
   price: 2
   )
 
@@ -48,6 +48,7 @@ aquarium_of_the_bay = Place.create(
   latitude: 37.808678,
   longitude: -122.409200,
   rating: 3.9,
+  duration: 120,
   price: 2
   )
 
@@ -78,6 +79,7 @@ cable_car = Place.create(
   latitude: 37.784814,
   longitude: -122.407740,
   rating: 3.5,
+  duration: 45,
   price: 1
   )
 
@@ -99,10 +101,11 @@ Photo.create(
 # Muir Woods
 muir_woods = Place.create(
   name: "Muir Woods National Monument",
-  description: "Redwoods",
+  description: "Redwoods by the bay",
   latitude: 37.895386,
   longitude: -122.578141,
   rating: 4.6,
+  duration: 300,
   price: 1
   )
 
@@ -138,6 +141,7 @@ sf_moma = Place.create(
   latitude: 37.785735,
   longitude: -122.400992,
   rating: 4.5,
+  duration: 120,
   price: 1
   )
 
@@ -161,25 +165,115 @@ Photo.create(
   image_url: "https://instagram.fsnc1-1.fna.fbcdn.net/t51.2885-15/e35/13109081_1723896514559583_614709634_n.jpg"
 )
 
-# battery_spencer = Place.create(
-#   name: "Battery Spencer",
-#   image_url: "https://instagram.fsnc1-1.fna.fbcdn.net/t51.2885-15/sh0.08/e35/p750x750/12724618_1571822593147746_505749575_n.jpg"
-#   )
+battery_spencer = Place.create(
+  name: "Battery Spencer",
+  description: "Battery Spencer description",
+  latitude: 37.827698,
+  longitude: -122.481799,
+  rating: 4.2,
+  duration: 30,
+  price: 0
+  )
 
-# china_town = Place.create(
-#   name: "China Town",
-#   image_url: "https://instagram.fsnc1-1.fna.fbcdn.net/t51.2885-15/e35/1516888_166301770399020_702906097_n.jpg"
-#   )
+PlacesType.create(
+  place_id: battery_spencer.id,
+  type_id: park.id
+  )
 
-# sutro_tower = Place.create(
-#   name: "Sutro Tower",
-#   image_url: "https://instagram.fsnc1-1.fna.fbcdn.net/t51.2885-15/e35/12093482_626976757445643_1739892906_n.jpg"
-#   )
+PlacesType.create(
+  place_id: battery_spencer.id,
+  type_id: place_of_interest.id
+  )
 
-# alamo_square = Place.create(
-#   name: "Alamo Square",
-#   image_url: "https://instagram.fsnc1-1.fna.fbcdn.net/t51.2885-15/e35/12751408_259692627695424_311922490_n.jpg"
-#   )
+PlacesType.create(
+  place_id: battery_spencer.id,
+  type_id: tourist_destination.id
+  )
+
+Photo.create(
+  place_id: battery_spencer.id,
+  image_url: "https://instagram.fsnc1-1.fna.fbcdn.net/t51.2885-15/sh0.08/e35/p750x750/12724618_1571822593147746_505749575_n.jpg"
+)
+
+china_town = Place.create(
+  name: "China Town",
+  description: "China Town Description",
+  latitude: 37.793346,
+  longitude: -122.406139,
+  rating: 4.1,
+  duration: 120,
+  price: 1,
+  )
+
+PlacesType.create(
+  place_id: china_town.id,
+  type_id: place_of_interest.id
+  )
+
+PlacesType.create(
+  place_id: china_town.id,
+  type_id: tourist_destination.id
+  )
+
+Photo.create(
+  place_id: china_town.id,
+  image_url: "https://instagram.fsnc1-1.fna.fbcdn.net/t51.2885-15/e35/1516888_166301770399020_702906097_n.jpg"
+)
+
+alamo_square = Place.create(
+  name: "Alamo Square",
+  description: "Painted ladies and old houses with great view on SF",
+  latitude: 37.776269,
+  longitude: -122.432967,
+  rating: 4.2,
+  duration: 45,
+  price: 0
+  )
+
+PlacesType.create(
+  place_id: alamo_square.id,
+  type_id: park.id
+  )
+
+PlacesType.create(
+  place_id: alamo_square.id,
+  type_id: place_of_interest.id
+  )
+
+PlacesType.create(
+  place_id: alamo_square.id,
+  type_id: tourist_destination.id
+  )
+
+Photo.create(
+  place_id: alamo_square.id,
+  image_url: "https://instagram.fsnc1-1.fna.fbcdn.net/t51.2885-15/e35/12751408_259692627695424_311922490_n.jpg"
+)
+
+castro_district = Place.create(
+  name: "Castro District",
+  description: "Castro District Description",
+  latitude: 37.762000,
+  longitude: -122.434875,
+  rating: 4.1,
+  duration: 60,
+  price: 0
+  )
+
+PlacesType.create(
+  place_id: castro_district.id,
+  type_id: place_of_interest.id
+  )
+
+PlacesType.create(
+  place_id: castro_district.id,
+  type_id: tourist_destination.id
+  )
+
+Photo.create(
+  place_id: castro_district.id,
+  image_url: "https://instagram.fsnc1-1.fna.fbcdn.net/t51.2885-15/e35/13397451_285829265095213_2012320115_n.jpg"
+)
 
 # civic_center = Place.create(
 #   name: "Civic Center",
@@ -191,11 +285,6 @@ Photo.create(
 #   image_url: "https://instagram.fsnc1-1.fna.fbcdn.net/t51.2885-15/e35/12912355_267495800257880_1905434648_n.jpg"
 #   )
 
-# castro_district = Place.create(
-#   name: "Castro District",
-#   image_url: "https://instagram.fsnc1-1.fna.fbcdn.net/t51.2885-15/e35/13397451_285829265095213_2012320115_n.jpg"
-#   )
-
 # palace_of_fine_arts = Place.create(
 #   name: "Palace of Fine Arts",
 #   image_url: "https://instagram.fsnc1-1.fna.fbcdn.net/t51.2885-15/e35/11347790_1166531286695489_2021504333_n.jpg"
@@ -204,4 +293,9 @@ Photo.create(
 # transamerica_tower = Place.create(
 #   name: "Trans-america Tower",
 #   image_url: "https://instagram.fsnc1-1.fna.fbcdn.net/t51.2885-15/sh0.08/e35/p750x750/12825796_528342577326118_1262316311_n.jpg"
+#   )
+
+# sutro_tower = Place.create(
+#   name: "Sutro Tower",
+#   image_url: "https://instagram.fsnc1-1.fna.fbcdn.net/t51.2885-15/e35/12093482_626976757445643_1739892906_n.jpg"
 #   )

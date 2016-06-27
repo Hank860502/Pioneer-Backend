c1ba24a41a6948fbf6e290caa366ef40db2521e8
Andrew Lee <email": null>
2016-06-27 10:26:24 -0700

    finish photos migration

--- db/migrate/20160627172302_create_photos.rb
+++ db/migrate/20160627172302_create_photos.rb
@@ -1,6 +1,8 @@
 class CreatePhotos < ActiveRecord::Migration
   def change
     create_table :photos do |t|
+      t.string :image_url
+      t.integer :place_id
 
       t.timestamps null: false
     end

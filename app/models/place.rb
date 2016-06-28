4a01b3dae38242563333bbb0e78c419cbd130363
Robin Soubry <robin.soubry@gmail.com>
2016-06-28 11:24:56 -0700

    Add act_as_mappable association

--- app/models/place.rb
+++ app/models/place.rb
@@ -3,4 +3,9 @@ class Place < ActiveRecord::Base
   has_many :wishlists, through: :places_wishlists
   has_many :photos
   has_and_belongs_to_many :types
+
+  acts_as_mappable :default_units => :miles,
+                   :default_formula => :sphere,
+                   :lat => :lat,
+                   :lng => :lng
 end
